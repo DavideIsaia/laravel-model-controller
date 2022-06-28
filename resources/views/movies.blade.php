f3cfddbd687db66ee85f83bb05eeee972da7d487
DavideIsaia <davide.enjoy@hotmail.it>
2022-06-28 14:32:39 +0200

    visualizzati a schermo dettagli dei film

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,1 +1,13 @@
-<h1>prova</h1>
+<h1>Movies Database</h1>
+<ul>
+  {{-- {{ dd($movies) }} --}}
+  @foreach ($movies as $movie)
+      <li>
+          <h2>{{ $movie->title }}</h2>
+          <h3>Titolo originale: {{ $movie->original_title }}</h3>
+          <h4>Nazionalità: {{ $movie->nationality }}</h4>
+          <div>Uscito nelle sale il {{\Carbon\Carbon::parse($movie->date)->format('d/M/Y')}}</div>
+          <div>Voto: {{ $movie->vote }}/10</div>
+      </li>
+  @endforeach
+</ul>
